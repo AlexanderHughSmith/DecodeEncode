a10b2c9cb62acde37ee866ce25b3dcead29e0468
Alexander <smithalexander05@gmail.com>
2020-03-06 14:28:58 -0500

    Working version

--- Encode Decode/decode.cc
+++ Encode Decode/decode.cc
@@ -1,6 +1,8 @@
+//Alexander Smith
 #include <iostream>
 #include <fstream>
 #include <string.h>
+#include <sstream>
 
 using namespace std;
 
@@ -15,47 +17,34 @@ struct Decoded
 int main()
 {
 	int key;
-	Decoded encoded_data;
+	Decoded decoded_data;
 	char input_buffer[BUFFER_SIZE];
 
-	ifstream infile("coded_message.txt");
+	ifstream infile("coded_message.txt", ios::in | ios::binary);
 
 	if (!infile)
 	{
 		cerr << "Cannot open file."<<endl;
 		return 2;
 	}
-
-	cout <<"What do you want to decode: ";
-	cin>>key;
-	string dm = "";
 	char c;
-	int i = key * -1;
-	int zeros = 0;
 	if (infile.is_open())
 	{
-	
-        while(infile.good())
-        {
-        	i+=1;
-            infile.get(c);
-            /*
-            if((c-i-5)>64 || (c-i-5)<-134 && (c-i-5)>-160 || (c-i-5)==32)
-            {
-            	cout<<char(c-i-5);
-            }
-            */
-            cout<<"c: "<<c-i-5<<endl;
-            cout<<endl;
-            cout<<char(c-i-key)<<"-"<<int(c-i-key)<<endl;
-            if(int(c-i-key)==0)
-            	break;
+		while(infile.peek()!=EOF)
+		{
+			infile.read((char *) &decoded_data,sizeof(decoded_data));
+			//cout<<decoded_data.message<<endl;
+			for(int i=0; i < BUFFER_SIZE; i++)
+			{
+				decoded_data.message[i] = decoded_data.message[i]-decoded_data.key-i; //i + key + input_buffer[i];
+				//cout<<int(decoded_data.message[i])<<endl;
+			}
+			cout<<decoded_data.message<<endl;	
+		}
 
-    	}
-    	cout<<endl;
-    	cout<<"here"<<endl;
-    	cout<<dm<<endl;
-	}    
+		
+       
+    }   
 	infile.close();
 	return 0;
 }
